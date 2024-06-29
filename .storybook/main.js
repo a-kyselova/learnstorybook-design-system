92fb9aaa5ed0d356a9a30b4e197da3b76ca42de7
a-kyselova <114485056+a-kyselova@users.noreply.github.com>
2024-06-29 16:57:53 +0300

    ✅ Add Storybook addons for accessibility and code coverage (#4)
    * ✅ Add Storybook addons for accessibility and code coverage
    
    * 💚 github actions fix

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,6 +7,8 @@ const config = {
     '@storybook/addon-essentials',
     '@chromatic-com/storybook',
     '@storybook/addon-interactions',
+    '@storybook/addon-a11y',
+    '@storybook/addon-coverage',
   ],
   framework: {
     name: '@storybook/react-vite',
